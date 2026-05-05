f0b118e5a677d1c80abc8996845f98a6bf0c2b83
Serhiy Fedurtsya <owner@dreamys.studio>
2026-05-05 18:15:18 -0400

    feat: remove unused id in patch

--- src/main/java/studio/dreamys/prometheus/Patch.java
+++ src/main/java/studio/dreamys/prometheus/Patch.java
@@ -2,13 +2,11 @@ package studio.dreamys.prometheus;
 
 public class Patch {
     public String name;
-    public String id;
     public String classPath;
     public String repositoryName;
 
-    public Patch(String name, String id, String classPath, String repositoryName) {
+    public Patch(String name, String classPath, String repositoryName) {
         this.name = name;
-        this.id = id;
         this.classPath = classPath;
         this.repositoryName = repositoryName;
     }
